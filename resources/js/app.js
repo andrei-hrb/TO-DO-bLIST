92fed977864bc95e9870bdd93a5ef0fc25915acf
andrei-hrb <andrei_hrb@icloud.com>
2018-11-21 13:33:55 +0200

    kinda

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,29 @@
+const $document = $(document);
+$document.ready(() => {
+    const $addButton = $('#addButton');
+    const $content = $('#formContent');
+    const $tasks = $('#tasks');
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+    function addTask(text) {
+        if ($content.val() !== '') {
+            const $snippet = $(`<li class="list-group-item text-black-50 btn-lg text-center mx-auto task" id="task"><a href="#" class="remove">${text}</a></li>`).hide();
+            $tasks.append($snippet);
+            $snippet.slideDown('slow');
+            $content.val('');
+        }
+    }
 
-require('./bootstrap');
+    $addButton.on('click', () => {
+        addTask($content.val());
+    });
 
-window.Vue = require('vue');
+    $document.on('keydown', e => {
+        if (e.keyCode == 13) addTask($content.val());
+    });
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app'
+    $document.on("click", "a.remove", function () {
+        $(this).parent().slideUp('slow', () => {
+            $(this).parent().remove();
+        });
+    });
 });
---
+++ resources/views/index.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>TO DO bLIST</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous">
+
+    <!-- Custom style + fonts -->
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Concert+One">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+
+    <link rel="shortcut icon" href="favicon.ico">
+</head>
+
+<body>
+<div class="container">
+    <!-- The text -->
+    <h1 id="main-h1" class="my-2 text-center col-sm flex text-black-50">TO DO bLIST</h1>
+
+    <h3 id="main-text" class="my-3 text-center text-black-50">This is what you have to do: </h3>
+
+    <!-- Input Form -->
+    <div class="input-group my-3 input-group-lg mx-auto p-3 active" id="form">
+        <input type="text" class="form-control text-black-50 btn-outline-secondary" placeholder="New Task" aria-label="New Task"
+               aria-describedby="button-addon2" id="formContent">
+        <div class="input-group-append" id="addButton">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Add</button>
+        </div>
+    </div>
+
+    <!-- The task list -->
+    <ul class="list-group list-group-flush mx-auto w-75" id="tasks"></ul>
+</div>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<script src="js/app.js"></script>
+</body>
+
+</html>
